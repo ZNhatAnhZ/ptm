8eadaca1eae3cc71b64de6f80089d82ed610f4a7
ZNhatAnhZ <ntnhatanh2001@gmail.com>
2025-11-09 16:01:10 +0700

    implement task management features with filtering and statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Task} from "./components/Task.jsx";
 import './App.css'
+import styled from "styled-components";
+import {useState} from "react";
+import {CreateTaskArea} from "./components/CreateTaskArea.jsx";
+import {FilterArea} from "./components/FilterArea.jsx";
+import {Statistics} from "./components/Statistics.jsx";
+import {FilterEnum, TaskArrayKey} from "./constants/Enum.jsx";
+import {filterTasks, useLocalStorage} from "./utils/Utils.jsx";
+
+const AppArea = styled.div`
+    padding: 20px 32px;
+    border: 1px solid #e0e4ea;
+    border-radius: 1em;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    margin-left: auto;
+    margin-right: auto;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [tasks, setTasks] = useLocalStorage(TaskArrayKey, []);
+    const [filter, setFilter] = useState(FilterEnum.ALL);
+    const addTask = (description) => setTasks([...tasks, {description: description, isCompleted: false}]);
+    const updateTask = (currentDescription, newDescription, isCompleted, isDeleted) => {
+        if (isDeleted) {
+            setTasks(tasks.filter(task => task.description !== currentDescription));
+        } else {
+            setTasks(tasks.map(task => {
+                if (task.description === currentDescription) {
+                    task.description = newDescription;
+                    task.isCompleted = isCompleted;
+                    return task;
+                }
+                return task;
+            }));
+        }
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <AppArea>
+            {CreateTaskArea({addTask})}
+            {Statistics({tasks})}
+            {FilterArea({setFilter})}
+            {tasks.filter(element => filterTasks(filter, element))
+                .map((element) => Task({element, updateTask}))}
+        </AppArea>
+    )
+
 }
 
 export default App
