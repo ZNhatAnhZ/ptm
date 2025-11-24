4c342d972ac69784c6b8525b247fcaac423fd16a
ZNhatAnhZ <ntnhatanh2001@gmail.com>
2025-11-25 00:33:50 +0700

    edit function

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import {useColorScheme} from "../hooks/index.js";
 import {DarkColorScheme} from "../constants/index.js";
+import {useRef, useState} from "react";
 
 const TaskItem = styled.div`
     background-color: ${props => props.colorscheme === DarkColorScheme ? 'gray' : 'white'};
@@ -17,14 +18,34 @@ const TaskItem = styled.div`
 const Button = styled.button`
     margin-left: 0.5em;
     margin-right: 0.5em;
+    border: ${props => props.isHightlighted === true ? '2px solid black' : null};
 `;
 
 export default function Task({element, updateTask}) {
     const {colorScheme} = useColorScheme();
+    const [currentTaskDesc, setCurrentTaskDesc] = useState(element.description);
+    const [enableEdit, setEnableEdit] = useState(false);
+    const inputRef = useRef(null);
 
     return (
         <TaskItem colorscheme={colorScheme}>
-            <div>{element.description}</div>
+            <Button title="click to allow edit, reclick to apply the edit" onClick={() => {
+                if (enableEdit) {
+                    setEnableEdit(false);
+                    updateTask({
+                        currentDescription: element.description,
+                        newDescription: currentTaskDesc,
+                        isCompleted: element.isCompleted,
+                        isDeleted: element.isDeleted
+                    })
+                } else {
+                    setEnableEdit(true);
+                    inputRef.current.focus();
+                }
+            }} isHightlighted={enableEdit}>&#x270E;</Button>
+            <input ref={inputRef} value={currentTaskDesc} readOnly={!enableEdit} onChange={e => {
+                setCurrentTaskDesc(e.target.value);
+            }}></input>
             <div>
                 {element.isCompleted ? null: <Button onClick={() => updateTask({
                     currentDescription: element.description,
