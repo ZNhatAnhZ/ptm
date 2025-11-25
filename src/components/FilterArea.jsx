860daf556b5420de6483c9134011c946428448c5
ZNhatAnhZ <ntnhatanh2001@gmail.com>
2025-11-25 19:00:48 +0700

    useCallback and useMemo

--- src/components/FilterArea.jsx
+++ src/components/FilterArea.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {FilterEnum} from "../constants";
+import {useCallback} from "react";
 
 const FilterAreaDiv = styled.div`
     display: flex;
@@ -17,25 +18,34 @@ const Input = styled.input`
 `;
 
 export default function FilterArea({filter, setFilter}) {
+    const updateFilter = useCallback((prefixDesc, status, isSearchFilter) => {
+        return e => {
+            if (isSearchFilter) {
+                if (e.target.value !== filter.prefixDesc) {
+                    setFilter({prefixDesc: e.target.value, status: status})
+                }
+            } else {
+                if (prefixDesc !== filter.prefixDesc || status !== filter.status) {
+                    setFilter({prefixDesc: prefixDesc, status: status})
+                }
+            }
+        };
+    }, [filter.prefixDesc, filter.status, setFilter]);
+
     return <>
         <FilterAreaDiv>
-            <button onClick={() => {
-                setFilter({prefixDesc: filter.prefixDesc, status: FilterEnum.ALL})
-            }}>All
+            <button onClick={updateFilter(filter.prefixDesc, FilterEnum.ALL, false)}>
+                All
             </button>
-            <button onClick={() => {
-                setFilter({prefixDesc: filter.prefixDesc, status: FilterEnum.ACTIVE})
-            }}>Active
+            <button onClick={updateFilter(filter.prefixDesc, FilterEnum.ACTIVE, false)}>
+                Active
             </button>
-            <button onClick={() => {
-                setFilter({prefixDesc: filter.prefixDesc, status: FilterEnum.COMPLETED})
-            }}>Completed
+            <button onClick={updateFilter(filter.prefixDesc, FilterEnum.COMPLETED, false)}>
+                Completed
             </button>
         </FilterAreaDiv>
         <SearchAreaDiv>
-            <Input placeholder="Search tasks here" onChange={e => {
-                setFilter({prefixDesc: e.target.value, status: filter.status})
-            }}></Input>
+            <Input placeholder="Search tasks here" onChange={updateFilter(filter.prefixDesc, filter.status, true)}></Input>
         </SearchAreaDiv>
     </>;
 }
--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,9 +1,11 @@
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {DefaultFilters} from "../constants/Enum.js";
 
 export default function useFilter(items, conditions) {
     const [filter, setFilter] = useState(DefaultFilters);
-    const filteredItems = filter ? items.filter(element => conditions.every(filterFn => filterFn(filter, element))) : items;
+    const filteredItems = useMemo(() => {
+        return filter ? items.filter(element => conditions.every(filterFn => filterFn(filter, element))) : items;
+    }, [filter, items]);
 
     return {filter, setFilter, filteredItems};
 }
