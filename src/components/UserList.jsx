189c2adb457d106cbfea2b8be99d547f637f24f5
ZNhatAnhZ <ntnhatanh2001@gmail.com>
2025-12-01 18:04:39 +0700

    feat: enhance UserList component with filtering, sorting, and pagination logic in the local

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -13,9 +13,9 @@ const PaginationDev = styled.div`
 
 export default function UserList({filter}) {
     const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
     const [page, setPage] = useState(1);
     const [maxPage, setMaxPage] = useState(1);
-    const [totalUsers, setTotalUsers] = useState(0);
     const [order, setOrder] = useState('asc');
 
     const {
@@ -24,21 +24,37 @@ export default function UserList({filter}) {
         isError,
         error,
     } = useQuery({
-        queryKey: ['users', page, order],
+        queryKey: ['users'],
         queryFn: async () => {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${maxItemsPerPage}&_sort=name&_order=${order}`);
-            setTotalUsers(response?.headers?.["x-total-count"] || 0);
-            setMaxPage(Math.ceil(response?.headers?.["x-total-count"] / maxItemsPerPage))
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+            setMaxPage(Math.ceil(response.data.length / maxItemsPerPage));
+            setUsers(response.data);
             return response.data;
         },
     });
 
     useEffect(() => {
-        if (Array.isArray(data) && data.length > 0) {
-            const filteredUsers = typeof filter?.name === 'string' && filter?.name?.length > 0 ? data.filter(user => user.name.startsWith(filter.name)) : data
-            setUsers(filteredUsers);
+        if (Array.isArray(users)) {
+            const searchedUsers = typeof filter?.name === 'string' && filter?.name?.length > 0 ? users.filter(user => user.name.startsWith(filter.name)) : users
+            const filteredAndSortedUsers = searchedUsers.sort((a, b) => {
+                if (order === 'asc') {
+                    return a.name.localeCompare(b.name);
+                } else {
+                    return b.name.localeCompare(a.name);
+                }
+            });
+            const startIndex = (page - 1) * maxItemsPerPage;
+            const paginatedUsers = filteredAndSortedUsers.slice(startIndex, startIndex + maxItemsPerPage);
+            const maxPage = Math.max(1, Math.ceil(filteredAndSortedUsers.length / maxItemsPerPage));
+            setFilteredUsers(JSON.stringify(filteredUsers) !== JSON.stringify(paginatedUsers) ? paginatedUsers : filteredUsers);
+            setMaxPage(maxPage);
+            if (page > maxPage) {
+                setPage(maxPage);
+            } else if (page < 1) {
+                setPage(1);
+            }
         }
-    }, [data, filter])
+    }, [data, filter, order, page, users])
 
     const deleteUser = (id) => {
         setUsers(users.filter(user => user.id !== id));
@@ -55,7 +71,7 @@ export default function UserList({filter}) {
 
         return (
             <div>
-                <div>Total number of users in the DB: {totalUsers}</div>
+                <div>Total number of users in the DB: {users.length}</div>
                 <PaginationDev>
                     <button disabled={!(page > 1)} onClick={() => {
                         if (page > 1) {
@@ -72,7 +88,7 @@ export default function UserList({filter}) {
                 <button onClick={() => {
                     setOrder(order === 'asc' ? 'desc' : 'asc');
                 }}>Sort {order === 'asc' ? 'A-Z' : 'Z-A'}</button>
-                {users.map(user => <User key={user.id} id={user.id} email={user.email} name={user.name}
+                {filteredUsers.map(user => <User key={user.id} id={user.id} email={user.email} name={user.name}
                                          phone={user.phone}
                                          website={user.website}
                                          onDelete={() => deleteUser(user.id)}
